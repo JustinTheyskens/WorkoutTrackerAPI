45f892a358e3ba021fe8c86ff8432a8a8278f3f9
JustinTheyskens <justintheyskens@gmail.com>
2025-03-27 13:27:08 -0400

    Added jdbc Client

--- src/main/java/Justin/T/WorkoutTracker/Run/RunRepo.java
+++ src/main/java/Justin/T/WorkoutTracker/Run/RunRepo.java
@@ -1,6 +1,10 @@
 package Justin.T.WorkoutTracker.Run;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -13,48 +17,54 @@ public class RunRepo
 {
     private List<Run> runs = new ArrayList<Run>();
 
+    private static final Logger log = LoggerFactory.getLogger(RunRepo.class);
+    private final JdbcClient jdbcClient;
 
+    public RunRepo(JdbcClient jdbcClient)
+    {
+        this.jdbcClient = jdbcClient;
+    }
 
-//    List<Run> findAll()
-//    {
-//        return runs;
-//    }
-//
-//    Optional<Run> findById(int id)
-//    {
-//        return runs.stream()
-//                .filter(r -> r.id() == id)
-//                .findFirst();
-//    }
-//
-//    void create(Run run)
-//    {
-//        runs.add(run);
-//    }
-//
-//    void update(Run run, Integer id)
-//    {
-//        Optional<Run> existingRun = findById(id);
-//
-//        if (existingRun.isPresent())
-//        {
-//            runs.set(runs.indexOf(existingRun.get()), run);
-//        }
-//    }
-//
-//    void delete(Integer id)
-//    {
-//        runs.removeIf(r -> r.id() == id);
-//    }
-//
-//    @PostConstruct
-//    private void init()
-//    {
-//        Run run1 = new Run(1, "Fist Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.Outdoor);
-//        Run run2 = new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plus(2, ChronoUnit.HOURS), 10, Location.Outdoor);
-//        Run run3 = new Run(3, "Third Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 7, Location.Indoor);
-//        runs.add(run1);
-//        runs.add(run2);
-//        runs.add(run3);
-//    }
+    List<Run> findAll()
+    {
+        return runs;
+    }
+
+    Optional<Run> findById(int id)
+    {
+        return runs.stream()
+                .filter(r -> r.id() == id)
+                .findFirst();
+    }
+
+    void create(Run run)
+    {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id)
+    {
+        Optional<Run> existingRun = findById(id);
+
+        if (existingRun.isPresent())
+        {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id)
+    {
+        runs.removeIf(r -> r.id() == id);
+    }
+
+    @PostConstruct
+    private void init()
+    {
+        Run run1 = new Run(1, "Fist Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.Outdoor);
+        Run run2 = new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plus(2, ChronoUnit.HOURS), 10, Location.Outdoor);
+        Run run3 = new Run(3, "Third Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 7, Location.Indoor);
+        runs.add(run1);
+        runs.add(run2);
+        runs.add(run3);
+    }
 }
